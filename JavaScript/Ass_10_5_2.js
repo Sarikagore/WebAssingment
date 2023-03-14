5152428f4d9d04521a46f19c16b6fa6303838f04
Sarikagore <122372722+Sarikagore@users.noreply.github.com>
2023-03-14 14:10:56 +0530

    Ass_10_5_2.js

---
+++ JavaScript/Ass_10_5_2.js
@@ -0,0 +1,26 @@
+var Arr = [12,56,755,27,16,2,1104,65,88];
+var i = 0;
+
+while(i < Arr.length)
+{
+    switch(((Arr[i] % 3 == 0 && Arr[i] % 8 == 0))?1 :(Arr[i] % 3 == 0)? 2:(Arr[i] % 8 == 0)? 3:4)
+    {
+        case 1:
+                console.log(`${Arr[i]} Divisible by ${3} And ${8}`);
+                break;
+                
+        case 2:
+            console.log(`${Arr[i]} Is Divisible By Only ${3}`);
+            break;
+
+        case 3:
+            console.log(`${Arr[i]} Is Divisible By Only ${8}`);
+            break;
+
+        default:
+            console.log(`${Arr[i]} Is Not Divisible By Both ${3} And ${8}`);
+            break;
+
+    }
+    i++;
+}
