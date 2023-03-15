9d08a20800ba7fd96cde75f88eb56f72fc959d33
Sarikagore <122372722+Sarikagore@users.noreply.github.com>
2023-03-15 20:49:22 +0530

    Ass_11_3.js

---
+++ JavaScript/Ass_11_3.js
@@ -0,0 +1,8 @@
+var StrOne = "good development"; 
+var StrTwo = "Hello all";
+var StrThree = "learn";
+
+StrOne = StrOne.replace("good","web");
+StrTwo = StrTwo.replace("All","WEBx86_Students");
+StrThree = StrTwo.concat(" ",StrThree," ",StrOne);
+console.log(StrThree);
